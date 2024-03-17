9bf9483ec897bd0f520d6d322b8304e8b39ad01d
Aron Fannarsson <aron.orri@gmail.com>
2024-03-17 12:32:46 -0400

    Fixed mistake in malloc
    I was malloc-ing for int every time which only works when compiled as 32-bit, where int and int* are both 4 bytes

--- decaPointer.c
+++ decaPointer.c
@@ -5,23 +5,23 @@ int********** buildDecaPointer(int num)
 {
     int* a1 = malloc(sizeof(int));
     *a1 = num;
-    int** a2 = malloc(sizeof(int));
+    int** a2 = malloc(sizeof(int*));
     *a2 = a1;
-    int*** a3 = malloc(sizeof(int));
+    int*** a3 = malloc(sizeof(int*));
     *a3 = a2;
-    int**** a4 = malloc(sizeof(int));
+    int**** a4 = malloc(sizeof(int*));
     *a4 = a3;
-    int***** a5 = malloc(sizeof(int));
+    int***** a5 = malloc(sizeof(int*));
     *a5 = a4;
-    int****** a6 = malloc(sizeof(int));
+    int****** a6 = malloc(sizeof(int*));
     *a6 = a5;
-    int******* a7 = malloc(sizeof(int));
+    int******* a7 = malloc(sizeof(int*));
     *a7 = a6;
-    int******** a8 = malloc(sizeof(int));
+    int******** a8 = malloc(sizeof(int*));
     *a8 = a7;
-    int********* a9 = malloc(sizeof(int));
+    int********* a9 = malloc(sizeof(int*));
     *a9 = a8;
-    int********** a10 = malloc(sizeof(int));
+    int********** a10 = malloc(sizeof(int*));
     *a10 = a9;
     return a10;
 }
